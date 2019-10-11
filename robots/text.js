9deec33f06aadf80f24e0f5e3a64c19ff9f1d209
André Luiz Ximenes <andreluizximenes@gmail.com>
2019-10-11 15:41:10 +0200

    Alguns ajustes na arquitetura dos robôs

--- robots/text.js
+++ robots/text.js
@@ -13,6 +13,9 @@ const nlu = new NaturalLanguageUnderstandingV1({
 
 const state = require('./state');
 
+module.exports = robot;
+
+//MAIN FUNCTION
 async function robot() {
   const content = state.load();
 
@@ -23,86 +26,80 @@ async function robot() {
   await fetchKeywordsofAllSentences(content);
 
   state.save(content);
-
-  async function fetchContentFromWikipedia(content) {
-    const response = await Algorithmia.client(AlgorithmiaApiKey)
-      .algo("web/WikipediaParser/0.1.2?timeout=300")
-      .pipe({
-        "articleName": content.searchTerm,
-        "lang": content.lang
-      });
-
-    const originalContent = response.get();
-    content.sourceContentOriginal = originalContent.content;
-  }
-
-  function sanitizeContent(content) {
-    const withoutBlankLinesAndMarkdowns = removeBlankLinesAndMarkdowns(content.sourceContentOriginal);
-    const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdowns);
-    content.sourceContentSanitized = withoutDatesInParentheses;
-
-    function removeBlankLinesAndMarkdowns(text) {
-      const allLines = text.split('\n');
-      const withoutBlankLinesAndMarkdowns = allLines.filter((line) => {
-        if (line.trim().length === 0 || line.trim().startsWith('=')) {
-          return false;
-        }
-        return true;
-      });
-      return withoutBlankLinesAndMarkdowns.join(' ');
-    }
-
-    function removeDatesInParentheses(text) {
-      return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g, ' ')
-    }
-  }
-
-  function breakContentIntoSentences(content) {
-    content.sentences = [];
-    const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized);
-    sentences.forEach((sentence) => {
-      content.sentences.push({
-        text: sentence,
-        keywords: [],
-        images: []
-      });
-    })
-  }
-
-  function limitMaximiumSentences(content) {
-    content.sentences = content.sentences.slice(0, content.maximumSentences);
-  }
-
-  async function fetchKeywordsofAllSentences(content) {
-    for (const sentence of content.sentences) {
-      sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text);
-    }
-  }
-
-  async function fetchWatsonAndReturnKeywords(sentence) {
-    return new Promise((resolve, reject) => {
-      nlu.analyze({
-        text: sentence,
-        features: {
-          keywords: {}
-        }
-      }, (error, response) => {
-        if (error) {
-          reject(error)
-          return
-        }
-
-        const keywords = response.keywords.map((keyword) => {
-          return keyword.text
-        })
-
-        resolve(keywords)
-      })
-    })
-  }
 }
 
 
+async function fetchContentFromWikipedia(content) {
+  const response = await Algorithmia.client(AlgorithmiaApiKey)
+    .algo("web/WikipediaParser/0.1.2?timeout=300")
+    .pipe({
+      "articleName": content.searchTerm,
+      "lang": content.lang
+    });
 
+  const originalContent = response.get();
+  content.sourceContentOriginal = originalContent.content;
+}
 
-module.exports = robot;
+function sanitizeContent(content) {
+  const withoutBlankLinesAndMarkdowns = removeBlankLinesAndMarkdowns(content.sourceContentOriginal);
+  const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdowns);
+  content.sourceContentSanitized = withoutDatesInParentheses;
+
+  function removeBlankLinesAndMarkdowns(text) {
+    const allLines = text.split('\n');
+    const withoutBlankLinesAndMarkdowns = allLines.filter((line) => {
+      if (line.trim().length === 0 || line.trim().startsWith('=')) {
+        return false;
+      }
+      return true;
+    });
+    return withoutBlankLinesAndMarkdowns.join(' ');
+  }
+
+  function removeDatesInParentheses(text) {
+    return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g, ' ')
+  }
+}
+
+function breakContentIntoSentences(content) {
+  content.sentences = [];
+  const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized);
+  sentences.forEach((sentence) => {
+    content.sentences.push({
+      text: sentence,
+      keywords: [],
+      images: []
+    });
+  })
+}
+
+function limitMaximiumSentences(content) {
+  content.sentences = content.sentences.slice(0, content.maximumSentences);
+}
+
+async function fetchKeywordsofAllSentences(content) {
+  for (const sentence of content.sentences) {
+    sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text);
+  }
+}
+
+async function fetchWatsonAndReturnKeywords(sentence) {
+  return new Promise((resolve, reject) => {
+    nlu.analyze({
+      text: sentence,
+      features: {
+        keywords: {}
+      }
+    }, (error, response) => {
+      if (error) {
+        reject(error)
+        return
+      }
+      const keywords = response.keywords.map((keyword) => {
+        return keyword.text
+      })
+      resolve(keywords)
+    })
+  })
+}
--- robots/userInput.js
+++ robots/userInput.js
@@ -1,32 +1,34 @@
 const readline = require('readline-sync');
 const state = require('./state');
 
-async function robot() {
+const MAX_SENTENCES = 7;
+
+module.exports = robot;
+
+//MAIN FUNCTIOn
+function robot() {
   const content = {
-    maximumSentences: 7
+    maximumSentences: MAX_SENTENCES
   }
 
   content.searchTerm = askAndReturnSearchTerm();
   content.lang = askAndReturnLanguage();
   content.prefix = askAndReturnPrefix();
   state.save(content);
-
-  function askAndReturnSearchTerm() {
-    return readline.question('Type a Wikipedia search term: ')
-  }
-
-  function askAndReturnPrefix() {
-    const prefixes = ['Who is', 'What is', 'The history of']
-    const index = readline.keyInSelect(prefixes, 'Choose the prefix: ')
-    return prefixes[index];
-  }
-
-  function askAndReturnLanguage() {
-    const languages = ['PT', 'EN']
-    const index = readline.keyInSelect(languages, 'Choose preferred language: ')
-    return languages[index];
-  }
-
 }
 
-module.exports = robot;
+function askAndReturnSearchTerm() {
+  return readline.question('Type a Wikipedia search term: ')
+}
+
+function askAndReturnPrefix() {
+  const prefixes = ['Who is', 'What is', 'The history of']
+  const index = readline.keyInSelect(prefixes, 'Choose the prefix: ')
+  return prefixes[index];
+}
+
+function askAndReturnLanguage() {
+  const languages = ['PT', 'EN']
+  const index = readline.keyInSelect(languages, 'Choose preferred language: ')
+  return languages[index];
+}
